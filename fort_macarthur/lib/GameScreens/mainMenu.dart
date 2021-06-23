e95501027a920dec5c04a6ac6155ee2b02abe24b
John Sinnott <C00242525@itcarlow.ie>
2021-06-23 15:07:43 +0100

    Have added buttons for the Menu options

--- fort_macarthur/lib/GameScreens/mainMenu.dart
+++ fort_macarthur/lib/GameScreens/mainMenu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../device.dart';
+
 class MainMenu extends StatefulWidget {
   /*
   const MainMenu({Key? key}) : super(key: key);
@@ -12,6 +14,62 @@ class MainMenu extends StatefulWidget {
 class _MainMenuPageState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center());
+    return Scaffold(
+      backgroundColor: Device.backroundCOLOR,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Game title.
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Text(
+                'Main Menu',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  color: Colors.black,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 20.0,
+                      color: Colors.white,
+                      offset: Offset(0, 0),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            // Play button.
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 3,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Push and replace current screen (i.e MainMenu) with
+                  // SelectSpaceship(), so that player can select a spaceship.
+                  /* Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => // TODO add the next page here 
+                      ,
+                    ),
+                  ); */
+                },
+                child: Text('Play'),
+              ),
+            ),
+
+            // Options button.
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 3,
+              child: ElevatedButton(
+                onPressed: () {
+                  // TODO: Navigate to options screen.
+                },
+                child: Text('Options'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
