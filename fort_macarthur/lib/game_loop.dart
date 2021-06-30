50694f3136459a1656192e2884a6afa04d1b80e8
AllexisAlvarico <c00242855@itcarlow.ie>
2021-06-30 15:08:52 +0100

    added ammunitionManager class and added it to the game_loop

--- fort_macarthur/lib/game_loop.dart
+++ fort_macarthur/lib/game_loop.dart
@@ -1,25 +1,48 @@
 import 'dart:ui';
-
+import 'ammo.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
 import 'package:flutter/cupertino.dart';
 
 // main game loop. pan detector necessary for touch detection
-class GameLoop extends BaseGame with PanDetector {
+class GameLoop extends BaseGame with PanDetector, TapDetector {
   // function for loading in assets and initializing classes
+  bool isPressed = false;
+  var ammoManager = new AmmunitionManager();
   Future<void> onLoad() async {
     // put image loading, class initialization here
+  }
+
+  void onTapDown(TapDownInfo event) {
+    isPressed = true;
+    if (ammoManager.ammo > 0) {
+      ammoManager.onTapDown(event);
+    }
+  }
+
+  void onTapUp(TapUpInfo event) {
+    isPressed = false;
+  }
+
+  void onTapCancel() {
+    isPressed = false;
   }
 
   // updates game
   void update(double dt) {
     super.update(dt);
+    if (isPressed && ammoManager.ammo > 0) {
+      print("Fired");
+    } else if (ammoManager.ammo == 0) {
+      print("Ammo empty");
+    }
     // put anything to be updated such here
   }
 
   // renders objects to the canvas
   void render(Canvas canvas) {
     super.render(canvas);
+    ammoManager.draw(canvas);
     // put anything to be drawn here
   }
 
