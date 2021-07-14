9ed57895511dbbbbbca9a2ff70953d3604c2aa78
John Sinnott <C00242525@itcarlow.ie>
2021-07-14 12:45:50 +0100

    Added a button for navigating to and from the options/menu

--- fort_macarthur/lib/Game/gamescreens/options.dart
+++ fort_macarthur/lib/Game/gamescreens/options.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'gameplay.dart';
-import 'options.dart';
+import 'mainmenu.dart';
 
 // Represents the main menu screen of Spacescape, allowing
 // players to start the game or modify in-game settings.
-class MainMenu extends StatelessWidget {
-  const MainMenu({Key? key}) : super(key: key);
+class Options extends StatelessWidget {
+  const Options({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +18,7 @@ class MainMenu extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 50.0),
               child: Text(
-                'Fort Missile',
+                'Options',
                 style: TextStyle(
                   fontSize: 50.0,
                   color: Colors.black,
@@ -35,44 +33,16 @@ class MainMenu extends StatelessWidget {
               ),
             ),
 
-            // Play button.
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 3,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Push and replace current screen (i.e MainMenu) with
-                  // SelectSpaceship(), so that player can select a spaceship.
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (context) => const GamePlay(),
-                    ),
-                  );
-                },
-                child: Text('Play'),
-              ),
-            ),
-
-            // Options button.
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 3,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (context) => const Options(),
-                    ),
-                  );
-                },
-                child: Text('Options'),
-              ),
-            ),
-
             // Exit button.
             SizedBox(
               width: MediaQuery.of(context).size.width / 3,
               child: ElevatedButton(
                 onPressed: () {
-                  SystemNavigator.pop();
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const MainMenu(),
+                    ),
+                  );
                 },
                 child: Text('Exit'),
               ),
